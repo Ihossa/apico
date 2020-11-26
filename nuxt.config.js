de9dc38667149f62deedeeed0a2a5e28b9d06793
ihor.hsymkov <i.shymkov@asferro.com>
2020-11-26 23:44:14 +0200

    add nojekil

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  mode: 'spa',
+  router: {
+     base: '/nuxt-apico/'
+   },
   head: {
     titleTemplate: '%s - apico',
     title: 'apico',
