9b1339830045ba2a7621ed2c147a81fd47e0b057
ihor.hsymkov <i.shymkov@asferro.com>
2020-11-26 22:02:54 +0200

    test version

---
+++ pages/AddProduct.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container class="container">
+    <v-card class="card-form">
+      <AddForm />
+    </v-card>
+  </v-container>
+</template>
+<script>
+import AddForm from "@/components/main/AddForm";
+
+export default {
+  layout: "main",
+  components: {
+    AddForm,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.card-form {
+  max-width: 1136px;
+  margin: auto;
+}
+</style>
